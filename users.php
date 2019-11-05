acd178c1b4ad6456e8dd624331c04ca5b94cb2c2
Tao309 <tao309@mail.ru>
2019-11-05 17:27:37 +0300

    Init users task #2

--- users.php
+++ users.php
@@ -1,1 +1,83 @@
 <?php
+
+
+/**
+ * Class User
+ *
+ * @property DB $db
+ * @property int $id
+ * @property string $name
+ * @property User $boss
+ */
+class User
+{
+	use UserObserver;
+	protected $db;
+
+	protected $id;
+	protected $name;
+	protected $boss;
+
+	public function __construct()
+	{
+		$this->db = new DB();
+	}
+
+	/**
+	 * @param int $id
+	 * @return User[]
+	 */
+	public static function getUsersByBossId(int $id):array
+	{
+		//Оставил инициализацию модели тут, для подхвата
+		$model = new User();
+
+		$model->findUsers(['boss_id' => $id]);
+	}
+}
+
+trait UserObserver
+{
+	protected function findUsers($params):array
+	{
+		$query = '
+		SELECT id, name
+		FROM users
+		WHERE
+		';
+
+		$w = [];
+		foreach($params as $index => $value)
+		{
+			$w[] = $index.' = '.$value;
+		}
+		$query .= implode(' AND ', $w);
+
+		//Абстрактный запрос в БД
+		$rows =  $this->db->findAll($query);
+
+		$users = [];
+
+		foreach($rows as $row)
+		{
+			$users[] = (new User())->initModel($row);
+		}
+
+		return $users;
+	}
+
+	private function initModel(array $data)
+	{
+		$this->id = $data['id'] ?? 0;
+		$this->name = $data['name'] ?? null;
+		$this->boss = $data['name'] ?? null;
+
+		return $this;
+	}
+}
+
+$users = User::getUsersByBossId(5);
+foreach($users as $user)
+{
+	echo $user->name;
+}
